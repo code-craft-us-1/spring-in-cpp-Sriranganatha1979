a0d3d5599270f501e2ceb07b4d57a699f6fc87c8
Natha, Sriranga <sriranga.natha@philips.com>
2024-08-26 22:55:37 +0530

    Code completion

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,28 @@
 #include "stats.h"
+#include <numeric>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+namespace Statistics 
+{
+    Stats ComputeStatistics(const std::vector<float>& numbers) {
+        Stats computedStats;
+        
+        if(numbers.size() == 0)
+        {
+            computedStats.average = NAN;
+            computedStats.max = NAN;
+            computedStats.min = NAN;
+            return computedStats;
+        }
+
+        // compute average of numbers and store it in computedStats.average
+        computedStats.average = std::accumulate(numbers.begin(), numbers.end(), 0.0) / numbers.size();
+
+        // compute max of numbers and store it in computedStats.max
+        computedStats.max = *std::max_element(numbers.begin(), numbers.end());
+
+        // compute min of numbers and store it in computedStats.min
+        computedStats.min = *std::min_element(numbers.begin(), numbers.end());
+
+        return computedStats;
+    }
 }
